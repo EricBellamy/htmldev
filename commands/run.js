102b43dd7663eb4b2b61313a72d363b61989b8f5
Eric Bellamy <ericbellamy@rogers.com>
2022-12-26 11:34:59 -0500

    add watch functionality

--- commands/run.js
+++ commands/run.js
@@ -1,21 +1,12 @@
 const fs = require('fs-extra');
 
-// Local
-const processFiles = require('../lib/build/files.js');
-const processHtml = require('../lib/build/html/html.js');
-
-module.exports = async function (PORT_NUMBER, EMPTY = true) {
+module.exports = async function (PORT_NUMBER, EMPTY = false) {
 	fs.ensureDirSync('dist');
 	if (EMPTY) fs.emptyDirSync('dist');
 
-	// Process the export files
-	await processFiles();
-
-	// Run build
-	await processHtml();
+	// Initialize watcher
+	require('../lib/build/watch.js')();
 
 	// Host HTTP server
 	require('../lib/server.js')(PORT_NUMBER);
-
-	// Initialize watcher
 }
